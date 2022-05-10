512e650c99218081d304b5ad9b0a770f5d7d4bbd
Carl <carlvalero14@gmail.com>
2022-05-10 17:32:36 +0800

    cropper

--- public/admin/addItem.php
+++ public/admin/addItem.php
@@ -19,6 +19,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin | EARIST LIBRARY</title>
 
+    
+<style>
+
+.image_area {
+  position: relative;
+}
+
+img {
+      display: block;
+      max-width: 100%;
+}
+
+.preview {
+      overflow: hidden;
+      width: 160px; 
+      height: 160px;
+      margin: 10px;
+      border: 1px solid red;
+}
+
+.modal-lg{
+      max-width: 1000px !important;
+}
+
+.overlay {
+  position: absolute;
+  bottom: 10px;
+  left: 0;
+  right: 0;
+  background-color: rgba(255, 255, 255, 0.5);
+  overflow: hidden;
+  height: 0;
+  transition: .5s ease;
+  width: 100%;
+}
+
+.image_area:hover .overlay {
+  height: 50%;
+  cursor: pointer;
+}
+
+.text {
+  color: #333;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+</style>
+   
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>        
+		<link rel="stylesheet" href="https://unpkg.com/dropzone/dist/dropzone.css" />
+		<link href="https://unpkg.com/cropperjs/dist/cropper.css" rel="stylesheet"/>
+		<script src="https://unpkg.com/dropzone"></script>
+		<script src="https://unpkg.com/cropperjs"></script>
+
     <?php require_once "../../cdn.php" ?>
 
     <link rel="stylesheet" href="../../css/header.css">
@@ -29,11 +91,13 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />   
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
-  
+
+    <script src="https://cdn.jsdelivr.net/gh/BossBele/cropzee@latest/dist/cropzee.js" defer></script>
            <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />  
            <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>  
            <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>             -->
+
 </head>
 <body>
     <header>
@@ -63,35 +127,46 @@
                     <div id="div-isbn" class="col-md-6 ">
                     </div>
                 </div>
-
+                
                 <div class="row g-4 mt-1">
+                    <!--TITLE-->
                     <div class="col-lg-5">
                         <label >Title</label>
                         <input type="text" class="form-control"  required>
                     </div>
+
+                    <!-- PUBLISHER -->
                     <div class="col-lg-4">
                         <label>Publisher</label>2          
                         <input type="text" class="form-control" placeholder="" required>
                     </div>
                     
+                    <!-- PUBLICATIONN DATE -->
                     <div class="col-lg-3">
                         <label >Published Date</label>
                         <input type="date" class="form-control" id="inputPassword2" placeholder="" required>
                     </div>
                 </div>
-        
+                
                 <div class="row g-4 mt-1">
-                   
-
-
+                    <!-- GENRE -->
                     <div class="col-md-4">
                         <label for="">Genre</label>
                         <input type="text" class="form-control" required>
                     </div>
+                    
+                    <!-- QUANTITY -->
+                    <div class="col-md-2">
+                        <label  class="">Quantity</label>
+                        <div class="col-1" style="width: 100px;">
+                          <input id="" min=1  type="number" class="form-control" required>
+                        </div>
+                    </div>
 
+                    <!-- EDITION -->
                     <div class="col-md-4 " >
                         <select id="select-edition" class="form-select col-2 p-0" style="border: none; max-width:120px; cursor:pointer ">
-                            <option style="" value="none" >No editions</option>
+                            <option  value="none" >No editions</option>
                             <option value="Volume">Volume</option>
                             <option value="Chapter">Chapter</option>
                             <option value="Series">Series</option>
@@ -102,25 +177,23 @@
                 </div>
 
 
-                <div class="row mt-2 g-2">
-
-                <label style="padding:12px 4px 12px 4px" class="col-auto">Quantity</label>
-                    <div class="col-1" style="width: 100px;">
-                        <input id="" min=1  type="number" class="form-control" required>
-                    </div>
-                </div>    
+             
                 
-                <div class="row g-4 mt-1">
+                <div class="row g-4 mt-2">
                     <label style="padding:12px 4px 12px 12px" class="col-auto">Number of Author/s</label>
                     <div class="col-1" style="width: 100px;">
                         <input id="author_count" min=1  max="10" type="number" class="form-control" required>
                     </div>
-                        
                 </div>
 
                 <div class="row"  id="author-input-container">
                 </div>
                 
+                <div class="mb-3 col-sm-4">
+                    <label for="formFile" >Image</label>
+                    <input class="form-control image" name="image"   type="file" accept="image/*"  id="upload_image">
+                </div>
+
                 <div>
                     <label for="">Description</label>
                     <textarea class="textarea mt-1" name="" id="" cols="30" rows="5"></textarea>
@@ -129,10 +202,40 @@
                 <div class="col-12">
                     <input class="btn btn-primary mt-1 col-12" type="submit" value="Submit">    
                 </div>
-
             </form>
+
+            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+			  	<div class="modal-dialog modal-lg" role="document">
+			    	<div class="modal-content">
+			      		<div class="modal-header">
+			        		<h5 class="modal-title">Crop Image Before Upload</h5>
+			        		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          			<span aria-hidden="true">×</span>
+			        		</button>
+			      		</div>
+			      		<div class="modal-body">
+			        		<div class="img-container">
+			            		<div class="row">
+			                		<div class="col-md-8">
+			                    		<img src="" id="sample_image" />
+			                		</div>
+			                		<div class="col-md-4">
+			                    		<div class="preview"></div>
+			                		</div>
+			            		</div>
+			        		</div>
+			      		</div>
+			      		<div class="modal-footer">
+			      			<button type="button" id="crop" class="btn btn-primary">Crop</button>
+			        		<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+			      		</div>
+			    	</div>
+			  	</div>
+			</div>	
         </div>
     </div>
+
+ 
 
     <script>
         $( "#nav-toggler" ).click(function() {
@@ -246,6 +349,76 @@
 
     </script>
 
+    <!-- CROPPERJS -->
 
+    
+<script>
+
+$(document).ready(function(){
+
+	var $modal = $('#modal');
+
+	var image = document.getElementById('sample_image');
+
+	var cropper;
+
+	$('#upload_image').change(function(event){
+		var files = event.target.files;
+
+		var done = function(url){
+			image.src = url;
+			$modal.modal('show');
+		};
+
+		if(files && files.length > 0)
+		{
+			reader = new FileReader();
+			reader.onload = function(event)
+			{
+				done(reader.result);
+			};
+			reader.readAsDataURL(files[0]);
+		}
+	});
+
+	$modal.on('shown.bs.modal', function() {
+		cropper = new Cropper(image, {
+			aspectRatio: 1,
+			viewMode: 3,
+			preview:'.preview'
+		});
+	}).on('hidden.bs.modal', function(){
+		cropper.destroy();
+   		cropper = null;
+	});
+
+	$('#crop').click(function(){
+		canvas = cropper.getCroppedCanvas({
+			width:400,
+			height:400
+		});
+
+		canvas.toBlob(function(blob){
+			url = URL.createObjectURL(blob);
+			var reader = new FileReader();
+			reader.readAsDataURL(blob);
+			reader.onloadend = function(){
+				var base64data = reader.result;
+				$.ajax({
+					url:'upload.php',
+					method:'POST',
+					data:{image:base64data},
+					success:function(data)
+					{
+						$modal.modal('hide');
+						$('#uploaded_image').attr('src', data);
+					}
+				});
+			};
+		});
+	});
+	
+});
+</script>
 </body>
 </html>
